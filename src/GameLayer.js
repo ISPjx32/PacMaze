d9b3049db502b02b1990a6addc2403454cf88107
Parinvut Rochanavedya <jmxz_1995@hotmail.com>
2015-02-28 07:02:26 +0700

    Added Pacman

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,10 +1,13 @@
 var GameLayer = cc.LayerColor.extend({
 	init: function(){
-		this._super(new cc.Color(127,127,127,255));
+		this._super(new cc.Color(0,0,0,0));
 		
 		this.maze = new Maze();
 		this.maze.setPosition( cc.p( 0, 40 ) );
         this.addChild( this.maze );
+
+        this.pacman = new Pacman( 10*40 + 20, 6*40 + 20 );
+        this.maze.addChild( this.pacman );
 
 		//this.scheduleUpdate();
 
